ce1671e3cd828356c440418b31355c2c9a86e4df
Arthur Valério <31370475+arthwrvl@users.noreply.github.com>
2024-02-15 10:10:08 -0300

    initial commit

---
+++ program.cpp
@@ -0,0 +1,24 @@
+//example program to be lexically analysed
+
+#include <stdio.h>
+
+int main(){
+    int num1;
+    int num2;
+    int soma;
+
+    printf("Digite um número: ");
+    scanf("%d", &num1);
+    printf("Digite o outro numero: ");
+    scanf("%d", &num2);
+
+    soma = num1 + num2;
+
+    if(soma < 0){
+        printf("a soma deu %d. um numero negativo", soma);
+    }else {
+        printf("a soma deu %d. um numero positivo", soma);
+    }
+
+    return 0;
+}
